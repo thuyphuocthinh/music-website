602d9546fda2a52d16d3e7de3ddd723fae39910b
thuyphuocthinh <104757603+thuyphuocthinh@users.noreply.github.com>
2024-07-21 17:24:26 +0700

    Created basic routes

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("OK");
+import express from "express";
+import { Express, Request, Response } from "express";
+import * as dotenv from "dotenv";
+
+// dotenv
+dotenv.config();
+
+const app: Express = express();
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.send("Hi");
+});
+
+const port: number | string = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`App is listening on ${port}`);
+});
